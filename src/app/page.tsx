2a98d9eae4bfbde3cc987fb8772c5a7adec80377
BlazeIsClone <sandevabeykoon123@gmail.com>
2025-01-09 13:45:47 +0530

    stash: print out data from api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,118 @@
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+export default async function Home() {
+  const homeRes = await fetch(`${process.env.API_URL}/settings/pages/home`, {
+    cache: "no-store",
+  });
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+  if (!homeRes.ok) {
+    throw new Error("Failed to fetch home settings");
+  }
+
+  const { data: homeData }: { data: PageHomeSettings } = await homeRes.json();
+
+  let itineraryDetails: Itinerary[] = [];
+
+  if (homeData.itinerary_items && homeData.itinerary_items.length > 0) {
+    const itineraryRes = await fetch(
+      `${process.env.API_URL}/modules/itinerary`
+    );
+
+    if (!itineraryRes.ok) {
+      throw new Error("Failed to fetch itinerary items");
+    }
+
+    const { data: itineraryData }: { data: Itinerary[] } =
+      await itineraryRes.json();
+
+    itineraryDetails = itineraryData.filter((item) =>
+      homeData.itinerary_items?.includes(item.id.toString())
+    );
+  }
+
+  return (
+    <article>
+      <section className="relative h-[400px]">
+        <div className="aboslute top-0 left-0 flex flex-col justify-center h-full">
+          {homeData.banner_image && (
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              className="absolute w-full h-full object-cover -z-10"
+              src={homeData.banner_image}
+              alt={"Banner image"}
+              width={500}
+              height={100}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          )}
+          <div className="text-center text-white">
+            {homeData.banner_content && (
+              <span
+                dangerouslySetInnerHTML={{ __html: homeData.banner_content }}
+              />
+            )}
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+      </section>
+      <section className="mt-8 p-8">
+        {homeData.itinerary_content && (
+          <span
+            dangerouslySetInnerHTML={{ __html: homeData.itinerary_content }}
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        )}
+        <div className="mt-4 grid grid-cols-4">
+          {itineraryDetails.length > 0 && (
+            <ul className="border rounded p-4">
+              {itineraryDetails.map((itinerary) => (
+                <li key={itinerary.id}>
+                  <Image
+                    className="w-full"
+                    src={itinerary.featured_image}
+                    alt={"Featured image"}
+                    width={500}
+                    height={100}
+                  />
+                  <h2>{itinerary.name}</h2>
+                  <p>{itinerary.short_description}</p>
+                  <p>Price: {itinerary.price}</p>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </section>
+    </article>
   );
+}
+
+interface PageHomeSettings {
+  banner_content?: string;
+  banner_image?: string;
+  itinerary_content?: string;
+  itinerary_items?: Array<string>;
+}
+
+interface Itinerary {
+  id: number;
+  status: number;
+  name: string;
+  slug: string;
+  price: string;
+  sale_price: string;
+  rating: number;
+  duration: string | null;
+  map: string;
+  is_sale_active: number;
+  code: string;
+  short_description: string;
+  overview: string | null;
+  terms_and_conditions: string | null;
+  package_includes: string | null;
+  featured_image: string;
+  listing_image?: string | null;
+  gallery?: string | null;
+  meta_title: string;
+  meta_description: string;
+  created_by: number;
+  updated_by: number | null;
+  created_at: string;
+  updated_at: string;
 }
