0265cac4c21797ce10d4f53b808677620e81bb55
BlazeIsClone <sandevabeykoon123@gmail.com>
2025-02-06 15:00:33 +0530

    add: api route for cache invalidation

---
+++ src/app/api/cache/[tag]/route.ts
@@ -0,0 +1,32 @@
+import { revalidateTag } from "next/cache";
+import { NextResponse } from "next/server";
+import { z } from "zod";
+
+const requestSchema = z.object({
+  tags: z.array(z.string()).nonempty("Tags array cannot be empty"),
+});
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const parsedData = requestSchema.parse(body);
+
+    parsedData.tags.forEach((tag) => revalidateTag(tag));
+
+    return NextResponse.json(
+      { message: `Cache purged for tags: ${parsedData.tags.join(", ")}` },
+      { status: 200 }
+    );
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      console.log(error.errors);
+
+      return NextResponse.json(
+        { message: "Failed to purge cache", error: error.message },
+        { status: 400 }
+      );
+    }
+
+    throw error;
+  }
+}
