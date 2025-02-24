0f968637d06497484665d06bc0347b37bb55f1e9
BlazeIsClone <sandevabeykoon123@gmail.com>
2025-02-24 06:44:17 +0530

    update: itienrary listing fliter by category

--- src/components/Itinerary/Listing/Aside.tsx
+++ src/components/Itinerary/Listing/Aside.tsx
@@ -1,5 +1,17 @@
+import { SearchItineraryCategories } from "@/components/Search/ItineraryCategories";
+
 export const ItineraryListingAside = () => (
   <aside>
-    <div className="rounded bg-slate-100 h-full"></div>
+    <div className="rounded-lg bg-slate-50 sticky top-10">
+      <div className="w-full p-4 flex flex-col gap-3">
+        <div>
+          <h2 className="font-semibold">Filter Itineraries</h2>
+        </div>
+        <div>
+          <label className="text-md">Itinerary Categories</label>
+          <SearchItineraryCategories />
+        </div>
+      </div>
+    </div>
   </aside>
 );
