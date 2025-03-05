3b11c407c777f5dfe9782ea52cde216bd9caa500
BlazeIsClone <sandevabeykoon123@gmail.com>
2025-03-05 17:51:44 +0530

    add: all itinerary index page & search

---
+++ src/app/itineraries/search/page.tsx
@@ -0,0 +1,116 @@
+import queryString from "query-string";
+import type { Metadata } from "next";
+import { z } from "zod";
+
+import { generateStaticParams } from "../page/[id]/page";
+import { ItineraryListing } from "@/components/Itinerary/Listing/Main";
+
+type Args = {
+  params: Promise<{
+    slug: string;
+  }>;
+  searchParams: Promise<{
+    query?: string;
+    destination?: string;
+    categories?: string;
+    audiences?: string;
+    duration?: string;
+  }>;
+};
+
+export const metadata: Metadata = {
+  robots: {
+    index: false,
+    follow: false,
+  },
+};
+
+export default async function Page({ params, searchParams }: Args) {
+  const { slug } = await params;
+  const { query, destination, categories, audiences, duration } =
+    await searchParams;
+
+  const data = await fetchData(
+    "1",
+    destination ?? slug,
+    query,
+    categories,
+    audiences,
+    duration
+  );
+
+  return <ItineraryListing destination={slug} {...data} />;
+}
+
+export { generateStaticParams };
+
+export const dynamic = "force-dynamic";
+
+const fetchData = async (
+  id: string,
+  destination: string,
+  search?: string,
+  categories?: string,
+  audiences?: string,
+  duration?: string
+): Promise<z.infer<typeof ApiResponseSchema>> => {
+  const query = queryString.stringify(
+    {
+      fields: ["id", "status", "slug"],
+      destination: destination,
+      search: search,
+      categories: categories?.split(","),
+      audiences: audiences?.split(","),
+      duration: duration,
+    },
+    { arrayFormat: "bracket" }
+  );
+
+  const response = await fetch(
+    `${process.env.API_URL}/modules/itinerary/index?page=${id}&${query}`,
+    {
+      next: {
+        tags: ["global"],
+      },
+    }
+  );
+
+  if (!response.ok) {
+    const errorMessage = `Failed to fetch: ${response.status} ${response.statusText}`;
+    throw new Error(errorMessage);
+  }
+
+  const data = await response.json();
+
+  try {
+    return ApiResponseSchema.parse(data);
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      console.log(error.errors);
+
+      throw new Error("API Response validation failed: " + error.message);
+    }
+    throw error;
+  }
+};
+
+const Schema = z.object({
+  id: z.number(),
+  status: z.number(),
+  slug: z.string(),
+});
+
+const ApiResponseSchema = z.object({
+  data: z.array(Schema).nullable(),
+  current_page: z.number().nullable(),
+  last_page: z.number().nullable(),
+  links: z
+    .array(
+      z.object({
+        url: z.string().nullable(),
+        label: z.string(),
+        active: z.boolean(),
+      })
+    )
+    .nullable(),
+});
