8801bd9d68b9d8a3601c8dfdfa7a01c59b4ad0e4
TharinduH <hettipower@gmail.com>
2025-07-16 09:11:43 +0530

    update : APIs functions updated

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -5,7 +5,12 @@ import {
     AccommodationData, 
     AccommodationCategoryData,
     ActivityData, 
-    ActivityCategoryData
+    ActivityCategoryData, 
+    AudiencesData,
+    CityData,
+    CountryData, 
+    DestinationData,
+    ItineraryCategoryData
 } from '@/types/interfaces';
 
 const baseURL = process.env.API_URL || '';
@@ -156,6 +161,181 @@ export async function fetchActivityCategories(): Promise<any> {
     while (hasNextPage) {
         
         const endpoint = `/modules/activityCategory/index?page=${page}&${query}`;
+    
+        try {
+            const data = await fetchData(endpoint);
+        
+            if (data?.data?.data?.length) {
+                allData.push(...data.data.data);
+            }
+        
+            hasNextPage = Boolean(data.next_page_url);
+            if (hasNextPage) page++;
+    
+        } catch (error) {
+            console.error(`Error fetching page ${page}:`, error);
+            break;
+        }
+    }
+    
+    return allData;
+}
+
+export async function fetchAudiences(): Promise<any> {
+
+    let page = 1;
+    let allData: AudiencesData[] = [];
+    let hasNextPage = true;
+    const query = queryString.stringify(
+        {
+            fields: ["id", "status", "name", "slug", "featured_image", "short_description", "meta_title", "meta_description"],
+        },
+        { arrayFormat: "bracket" }
+    );
+
+    while (hasNextPage) {
+        
+        const endpoint = `/modules/audience/index?page=${page}&${query}`;
+    
+        try {
+            const data = await fetchData(endpoint);
+        
+            if (data?.data?.data?.length) {
+                allData.push(...data.data.data);
+            }
+        
+            hasNextPage = Boolean(data.next_page_url);
+            if (hasNextPage) page++;
+    
+        } catch (error) {
+            console.error(`Error fetching page ${page}:`, error);
+            break;
+        }
+    }
+    
+    return allData;
+}
+
+export async function fetchCities(): Promise<any> {
+
+    let page = 1;
+    let allData: CityData[] = [];
+    let hasNextPage = true;
+    const query = queryString.stringify(
+        {
+            fields: ["id", "status", "name", "slug", "featured_image", "short_description", "meta_title", "meta_description"],
+        },
+        { arrayFormat: "bracket" }
+    );
+
+    while (hasNextPage) {
+        
+        const endpoint = `/modules/city/index?page=${page}&${query}`;
+    
+        try {
+            const data = await fetchData(endpoint);
+        
+            if (data?.data?.data?.length) {
+                allData.push(...data.data.data);
+            }
+        
+            hasNextPage = Boolean(data.next_page_url);
+            if (hasNextPage) page++;
+    
+        } catch (error) {
+            console.error(`Error fetching page ${page}:`, error);
+            break;
+        }
+    }
+    
+    return allData;
+}
+
+export async function fetchCountries(): Promise<any> {
+
+    let page = 1;
+    let allData: CountryData[] = [];
+    let hasNextPage = true;
+    const query = queryString.stringify(
+        {
+            fields: ["id", "status", "name", "slug", "featured_image", "short_description", "meta_title", "meta_description"],
+        },
+        { arrayFormat: "bracket" }
+    );
+
+    while (hasNextPage) {
+        
+        const endpoint = `/modules/country/index?page=${page}&${query}`;
+    
+        try {
+            const data = await fetchData(endpoint);
+        
+            if (data?.data?.data?.length) {
+                allData.push(...data.data.data);
+            }
+        
+            hasNextPage = Boolean(data.next_page_url);
+            if (hasNextPage) page++;
+    
+        } catch (error) {
+            console.error(`Error fetching page ${page}:`, error);
+            break;
+        }
+    }
+    
+    return allData;
+}
+
+export async function fetchDestinations(): Promise<any> {
+
+    let page = 1;
+    let allData: DestinationData[] = [];
+    let hasNextPage = true;
+    const query = queryString.stringify(
+        {
+            fields: ["id", "status", "name", "slug", "featured_image", "short_description", "meta_title", "meta_description"],
+        },
+        { arrayFormat: "bracket" }
+    );
+
+    while (hasNextPage) {
+        
+        const endpoint = `/modules/destination/index?page=${page}&${query}`;
+    
+        try {
+            const data = await fetchData(endpoint);
+        
+            if (data?.data?.data?.length) {
+                allData.push(...data.data.data);
+            }
+        
+            hasNextPage = Boolean(data.next_page_url);
+            if (hasNextPage) page++;
+    
+        } catch (error) {
+            console.error(`Error fetching page ${page}:`, error);
+            break;
+        }
+    }
+    
+    return allData;
+}
+
+export async function fetchItineraryCategories(): Promise<any> {
+
+    let page = 1;
+    let allData: DestinationData[] = [];
+    let hasNextPage = true;
+    const query = queryString.stringify(
+        {
+            fields: ["id", "status", "name", "slug", "featured_image", "description", "meta_title", "meta_description"],
+        },
+        { arrayFormat: "bracket" }
+    );
+
+    while (hasNextPage) {
+        
+        const endpoint = `/modules/itineraryCategory/index?page=${page}&${query}`;
     
         try {
             const data = await fetchData(endpoint);
