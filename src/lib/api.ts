31e0c99363c825343ac58634a5fb9dd8b83f8a5c
TharinduH <hettipower@gmail.com>
2025-07-16 08:58:21 +0530

    add : API request functions

---
+++ src/lib/api.ts
@@ -0,0 +1,177 @@
+import axios from 'axios';
+import queryString from "query-string";
+
+import { 
+    AccommodationData, 
+    AccommodationCategoryData,
+    ActivityData, 
+    ActivityCategoryData
+} from '@/types/interfaces';
+
+const baseURL = process.env.API_URL || '';
+
+export const axiosInstance = axios.create({
+    baseURL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+export async function fetchData(endpoint: string): Promise<any> {
+    try {
+        console.log(`Making request to: ${axiosInstance.defaults.baseURL}${endpoint}`);
+        const res = await axiosInstance.get(endpoint);
+        console.log('Response status:', res.status);
+        return res || false;
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            console.error('API request failed:', error.message);
+        } else {
+            console.error('API request failed:', error);
+        }
+        if (typeof error === 'object' && error !== null && 'response' in error) {
+            console.error('Response :', error);
+        }
+        return [];
+    }
+}
+
+export async function fetchAccommodations(): Promise<any> {
+
+    let page = 1;
+    let allData: AccommodationData[] = [];
+    let hasNextPage = true;
+    const query = queryString.stringify(
+        {
+            fields: ["id", "status", "name", "slug", "short_description", "meta_title", "meta_description", "featured_image"],
+        },
+        { arrayFormat: "bracket" }
+    );
+
+    while (hasNextPage) {
+        
+        const endpoint = `/modules/accommodation/index?page=${page}&${query}`;
+    
+        try {
+            const data = await fetchData(endpoint);
+        
+            if (data?.data?.data?.length) {
+                allData.push(...data.data.data);
+            }
+        
+            hasNextPage = Boolean(data.next_page_url);
+            if (hasNextPage) page++;
+    
+        } catch (error) {
+            console.error(`Error fetching page ${page}:`, error);
+            break;
+        }
+    }
+    
+    return allData;
+}
+
+export async function fetchAccommodationCategories(): Promise<any> {
+
+    let page = 1;
+    let allData: AccommodationCategoryData[] = [];
+    let hasNextPage = true;
+    const query = queryString.stringify(
+        {
+            fields: ["id", "status", "name", "slug", "featured_image"],
+        },
+        { arrayFormat: "bracket" }
+    );
+
+    while (hasNextPage) {
+        
+        const endpoint = `/modules/accommodationCategory/index?page=${page}&${query}`;
+    
+        try {
+            const data = await fetchData(endpoint);
+        
+            if (data?.data?.data?.length) {
+                allData.push(...data.data.data);
+            }
+        
+            hasNextPage = Boolean(data.next_page_url);
+            if (hasNextPage) page++;
+    
+        } catch (error) {
+            console.error(`Error fetching page ${page}:`, error);
+            break;
+        }
+    }
+    
+    return allData;
+}
+
+export async function fetchActivities(): Promise<any> {
+
+    let page = 1;
+    let allData: ActivityData[] = [];
+    let hasNextPage = true;
+    const query = queryString.stringify(
+        {
+            fields: ["id", "status", "name", "slug", "featured_image", "duration", "short_description", "meta_title", "meta_description"],
+        },
+        { arrayFormat: "bracket" }
+    );
+
+    while (hasNextPage) {
+        
+        const endpoint = `/modules/activity/index?page=${page}&${query}`;
+    
+        try {
+            const data = await fetchData(endpoint);
+        
+            if (data?.data?.data?.length) {
+                allData.push(...data.data.data);
+            }
+        
+            hasNextPage = Boolean(data.next_page_url);
+            if (hasNextPage) page++;
+    
+        } catch (error) {
+            console.error(`Error fetching page ${page}:`, error);
+            break;
+        }
+    }
+    
+    return allData;
+}
+
+export async function fetchActivityCategories(): Promise<any> {
+
+    let page = 1;
+    let allData: ActivityCategoryData[] = [];
+    let hasNextPage = true;
+    const query = queryString.stringify(
+        {
+            fields: ["id", "status", "name", "slug", "description", "meta_title", "meta_description", "featured_image"],
+        },
+        { arrayFormat: "bracket" }
+    );
+
+    while (hasNextPage) {
+        
+        const endpoint = `/modules/activityCategory/index?page=${page}&${query}`;
+    
+        try {
+            const data = await fetchData(endpoint);
+        
+            if (data?.data?.data?.length) {
+                allData.push(...data.data.data);
+            }
+        
+            hasNextPage = Boolean(data.next_page_url);
+            if (hasNextPage) page++;
+    
+        } catch (error) {
+            console.error(`Error fetching page ${page}:`, error);
+            break;
+        }
+    }
+    
+    return allData;
+}
