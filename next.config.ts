127c303df296d9643476c7d988a78283e8571fa7
BlazeIsClone <sandevabeykoon123@gmail.com>
2025-10-30 11:52:08 +0530

    config: next image optimize path

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
+const apiUrl = process.env.NEXT_PUBLIC_API_URL || "";
+const domain = new URL(apiUrl).hostname;
+
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { protocol: "http", hostname: "localhost" },
-      { protocol: "https", hostname: "*.mayadev.xyz" },
+      { protocol: "https", hostname: domain },
     ],
   },
 };
